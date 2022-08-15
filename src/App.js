ceabff28c041101c14ebf0649c0ab9bb2b1feda1
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-08-15 13:16:27 -0300

    updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
   <ModeProvider> 
       <Routes>
-        <Route exact path="/frontend-countries" element = {<Homepage />} />
+        <Route path="/frontend-countries" element = {<Homepage />} />
         <Route path="/frontend-countries/country" element = {<CountryPage />} />
       </Routes>
   </ModeProvider>
