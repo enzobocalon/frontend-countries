c2b7e0103d8714776d514edb7e068c29656248cd
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-08-15 12:54:53 -0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Homepage from './pages/Homepage';
 import ModeProvider from './context/Modes';
 import CountryPage from './pages/CountryPage';
@@ -8,12 +8,10 @@ import CountryPage from './pages/CountryPage';
 function App() {
   return (
   <ModeProvider> 
-    <Router>
       <Routes>
         <Route path="/" element = {<Homepage />} />
         <Route path="/country" element = {<CountryPage />} />
       </Routes>
-    </Router>
   </ModeProvider>
   );
 }
